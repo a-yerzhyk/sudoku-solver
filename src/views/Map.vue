31105515b3e896b19f1bb28270d68fec785f9a89
a-yerzhyk <andriiyerzh@gmail.com>
2022-10-15 17:56:33 +0300

    Remove console.log

--- src/views/Map.vue
+++ src/views/Map.vue
@@ -3,7 +3,6 @@ import { ref } from 'vue';
 import SudokuMap from '../classes/sudoku-map'
 
 const sudokuMap = ref<SudokuMap>(new SudokuMap())
-console.log(sudokuMap.value.bigSquareMap)
 </script>
 
 <template>
